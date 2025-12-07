d96e81cad2f699558a1dfd23918a43cacc27d73d
kaichiehuang <kaichiehuang@gmail.com>
2025-12-07 00:50:12 -0800

    12/6

--- 621.task-scheduler.py
+++ 621.task-scheduler.py
@@ -5,8 +5,31 @@
 #
 
 # @lc code=start
+import heapq
+from collections import deque, Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-n for n in count.values()]
+        heapq.heapify(maxHeap)
+        time = 0
+        q = deque()
+
+        while maxHeap or q:
+            time += 1
+
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+
+            if q and q[0][1] == time:
+                cnt = q.popleft()[0]
+                heapq.heappush(maxHeap, cnt)
+
+        return time
+                    
+
         
 # @lc code=end
 
