5e99bd24c532e92b36dd23f9dd337e66ec5dbe8c
kaichiehuang <kaichiehuang@gmail.com>
2025-12-28 17:47:03 -0800

    12/27

--- 338.counting-bits.py
+++ 338.counting-bits.py
@@ -7,14 +7,11 @@
 # @lc code=start
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n+1)
-        offset = 1
+        ans = [0] * (n+1)
 
-        for i in range(1, n+1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i-offset]
-
-        return dp
+        for i in range(n+1):
+            ans[i] = ans[i >> 1] + (i&1)
+        
+        return ans
 # @lc code=end
 
